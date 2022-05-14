e8f7ea6c56365e657141b1d7bd8095dd8d0f4b2a
TalkativeDiv <85126640+TalkativeDiv@users.noreply.github.com>
2022-05-13 22:08:30 -0400

    Added a new page 🧑‍🚀 + more

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,11 +3,12 @@ const menuOpen = document.querySelector(".menu");
 const menuClose = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 const BASE_URL = "http://api.open-notify.org/iss-now.json";
+const astrosURL = "http://api.open-notify.org/astros.json";
 const mapURL =
   "https://api.mapbox.com/styles/v1/talkativediv/cl2xp846t003c14mxg639tkec.html?title=false&access_token=pk.eyJ1IjoidGFsa2F0aXZlZGl2IiwiYSI6ImNsMnhwMjc3ejAxb2wzaW1odHV5b2FzYmkifQ.FIv60aJfQGwZeTMhQvtTpg&amp;zoomwheel=false#9//";
 const mapISS = document.getElementById("mapISS");
 //functions
-let getApi = async () => {
+let getISSLocation = async () => {
   // Storing response
   const response = await fetch(BASE_URL)
     .then((response) => response.json())
@@ -18,6 +19,15 @@ let getApi = async () => {
       );
     });
 };
+
+let getIntOfAstros = async () => {
+  const response = await fetch(astrosURL)
+  .then((response) => response.json())
+  .then((data) => {
+     document.getElementById("lblAstrosInSpace").innerHTML = data.number;
+  });
+}
+
 let getEmbedURL = (lat, long) => {
   return `https://api.mapbox.com/styles/v1/talkativediv/cl2xp846t003c14mxg639tkec.html?title=false&access_token=pk.eyJ1IjoidGFsa2F0aXZlZGl2IiwiYSI6ImNsMnhwMjc3ejAxb2wzaW1odHV5b2FzYmkifQ.FIv60aJfQGwZeTMhQvtTpg&amp;zoomwheel=false#9/${lat}/${long}`;
 };
@@ -29,4 +39,4 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
-setInterval(getApi, 1000);
+
